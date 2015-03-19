b0f578ce9aeca7208937908ac889bbdbef19042b
robot527 <luoyaozu@foxmail.com>
2015-03-19 20:27:14 +0800

    updated quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,6 +1,14 @@
 #! /usr/bin/python
 
 
+def generate_random_integer_list(n, stop=100):
+    from random import randrange
+    lst = []
+    for i in range(n):
+        lst.append(randrange(stop))
+    return lst
+
+
 def quick_sort(lst):
     if len(lst) <= 1:
         return lst
@@ -11,10 +19,11 @@ def quick_sort(lst):
             + quick_sort([item for item in lst[1:] if item >= pivot])
 
 
-list1 = [5, 3, 7, 4, 1, 9, 8, 6, 2]
-list2 = quick_sort(list1)
-
-
-print "Before: ", list1
-print "After: ", list2
-
+if __name__ == '__main__':
+    num = 20
+    list1 = generate_random_integer_list(num, num * 10)
+    print "Data are as follows:"
+    print list1
+    list2 = quick_sort(list1)
+    print "After sorting: "
+    print list2
